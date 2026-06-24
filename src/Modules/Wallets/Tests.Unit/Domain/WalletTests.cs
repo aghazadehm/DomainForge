b4a0863647fa3be5a8e3f6c981a9a153b844f509
Mohammad Aghazadeh <aghazadeh.m@gmail.com>
2026-06-24 23:27:31 +0330

    test(wallets): add wallet aggregate test skeleton

---
+++ src/Modules/Wallets/Tests.Unit/Domain/WalletTests.cs
@@ -0,0 +1,14 @@
+using DomainForge.Modules.Wallets.Domain.ValueObjects;
+using DomainForge.Modules.Wallets.Domain.States;
+using Xunit;
+
+namespace DomainForge.Modules.Wallets.Tests.Unit.Domain;
+
+public class WalletTests
+{
+    [Fact]
+    public void Placeholder_for_wallet_aggregate_tests()
+    {
+        Assert.True(true);
+    }
+}
